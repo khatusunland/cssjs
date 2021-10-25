6075800258c0ba99aa7327d13c57d3f34d399e7a
khatusunland <61135396+khatusunland@users.noreply.github.com>
2021-10-25 15:19:51 +0700

    Update formfaceseo-2020.js

--- formfaceseo-2020.js
+++ formfaceseo-2020.js
@@ -111,7 +111,7 @@ jQuery(divpopup).attr('id', 'myModalthanh');
 jQuery(divpopup).attr('class', 'thanhfaceseo-modal');
 jQuery( "body" ).append(divpopup);
 var titleform='Đăng ký Form';
-var urlform="https://docs.google.com/forms/d/e/1FAIpQLSfLmBC0f3ipvca5eZeH48HotuzHmbF96XAMhjaMhOFKjmerRg/formResponse";
+var urlform="https://docs.google.com/forms/u/0/d/e/1FAIpQLScgkyHXqBoiQosuLI6w7Z3TpB6HSZj3yoNIg-KLdwQeqL7n1g/formResponse";
 var urlchuyen=window.location.href;
 var hovaten="888852917";
 var dienthoai="383558965";
